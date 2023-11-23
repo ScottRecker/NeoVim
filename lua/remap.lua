b16b33b5877985811e30608aface5ef6b4feeb6f
Scott Recker <scott.a.recker@gmail.com>
2023-11-22 21:11:51 -0600

    Add key remaps

---
+++ lua/remap.lua
@@ -0,0 +1,67 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+    -- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts) -- left window
+keymap("n", "<C-k>", "<C-w>k", opts) -- up window
+keymap("n", "<C-j>", "<C-w>j", opts) -- down window
+keymap("n", "<C-l>", "<C-w>l", opts) -- right window
+
+-- Resize with arrows when using multiple windows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-down>", ":resize +2<cr>", opts)
+keymap("n", "<C-right>", ":vertical resize -2<cr>", opts)
+keymap("n", "<C-left>", ":vertical resize +2<cr>", opts)
+
+
+vim.g.mapleader = " "
+keymap("n", "<leader>pv", vim.cmd.Ex)
+
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
+
+-- greatest remap ever
+keymap("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+keymap({"n", "v"}, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
+
+keymap({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+keymap("i", "jj", "<Esc>")
+
+keymap("n", "Q", "<nop>")
+keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap("n", "<leader>f", vim.lsp.buf.format)
+
+keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+--keymap("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+--keymap("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+keymap("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
