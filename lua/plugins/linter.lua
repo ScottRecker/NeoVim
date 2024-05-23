392b084d3e9375420f95ab04632ded112858a560
Scott Recker <scott.a.recker@gmail.com>
2024-05-23 13:45:15 -0500

    Spacing fix

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -1,19 +1,19 @@
 return {
-	-- https://github.com/mfussenegger/nvim-lint
-	"mfussenegger/nvim-lint",
+  -- https://github.com/mfussenegger/nvim-lint
+  "mfussenegger/nvim-lint",
 
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lint = require("lint")
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
-			python = { "pylint" },
-		}
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      python = { "pylint" },
+    }
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
