3ad5ec365f45b6418fb4b3f864212d87da1bf48c
Scott Recker <scott.a.recker@gmail.com>
2024-06-04 18:24:55 -0500

    Whitespace fixup

--- lua/plugins/nvim-dap-python.lua
+++ lua/plugins/nvim-dap-python.lua
@@ -1,17 +1,17 @@
 return {
-	-- https://github.com/mfussenegger/nvim-dap-python
-	"mfussenegger/nvim-dap-python",
-	ft = "python",
-	dependencies = {
-		-- https://github.com/mfussenegger/nvim-dap
-		"mfussenegger/nvim-dap",
-	},
-	config = function()
-		local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-		require("dap-python").setup(path)
-		-- Update the path passed to setup to point to your system or virtual env python binary
-		-- require('dap-python').setup('.venv/bin/python')
-		-- require('dap-python').setup('python')
-		-- require('dap-python').setup()
-	end,
+  -- https://github.com/mfussenegger/nvim-dap-python
+  "mfussenegger/nvim-dap-python",
+  ft = "python",
+  dependencies = {
+    -- https://github.com/mfussenegger/nvim-dap
+    "mfussenegger/nvim-dap",
+  },
+  config = function()
+    local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+    require("dap-python").setup(path)
+    -- Update the path passed to setup to point to your system or virtual env python binary
+    -- require('dap-python').setup('.venv/bin/python')
+    -- require('dap-python').setup('python')
+    -- require('dap-python').setup()
+  end,
 }
